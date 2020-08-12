2adbca22e4394b3d76dca0d786ea31322e98df13
Mohamed Magde Mohamed <mohamedmagdemohamed@MacBook-Pro.local>
2020-08-12 10:07:24 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService],
 })
 export class AppComponent {
   title = 'todo-app';
+  private todoDataService: TodoDataService;
+  newTodo: Todo = new Todo();
+  constructor(todoDataService: TodoDataService) {}
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
